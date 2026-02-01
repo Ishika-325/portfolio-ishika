1b58d1e560ddb2ef6b9e3e2ae1c44e2e31af5a4a
Ishika <gishika325@gmail.com>
2026-02-01 20:32:30 +0530

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Profile,Skills,Projects
+from .models import Profile,Skills,Projects, Contact
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Skills)
 admin.site.register(Projects)
+admin.site.register(Contact)
+
