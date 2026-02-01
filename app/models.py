6f9f23f66f9154075b878cbe66c27ea97e5f4e6a
Ishika <gishika325@gmail.com>
2026-02-01 20:30:17 +0530

    Add Contact model for user messages

--- app/models.py
+++ app/models.py
@@ -36,9 +36,14 @@ class Projects(models.Model):
     def __str__(self):
         return f"displayed {self.title}" 
     
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
-
-
+    def __str__(self):
+        return f"message from {self.name}"
 
 
 
