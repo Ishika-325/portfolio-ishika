398b20c8cf15a53aa50c838e83b2e0c79d02b839
Ishika <gishika325@gmail.com>
2026-02-01 20:29:10 +0530

    update urls.py to add project url

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
       path('', views.home, name='home'),
+      path("projects/", views.all_projects, name="projects"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
